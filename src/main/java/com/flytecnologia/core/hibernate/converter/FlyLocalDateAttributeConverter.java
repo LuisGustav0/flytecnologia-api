a1d0c459fd6c17eef9fba4ff969359242a6fae81
Jullierme Barros <julliermegyn@gmail.com>
2018-03-26 22:36:46 -0300

    refectory

--- src/main/java/com/flytecnologia/core/hibernate/converter/FlyLocalDateAttributeConverter.java
+++ src/main/java/com/flytecnologia/core/hibernate/converter/FlyLocalDateAttributeConverter.java
@@ -6,7 +6,7 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 @Converter(autoApply = true)
-public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
+public class FlyLocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
 
     @Override
     public Date convertToDatabaseColumn(LocalDate locDate) {
