fbadc0e8a0896cae12101ec43512befa40ea0b35
Jullierme Barros <julliermegyn@gmail.com>
2018-07-22 09:55:09 -0300

    refectory

--- src/main/java/com/flytecnologia/core/user/FlyUser.java
+++ src/main/java/com/flytecnologia/core/user/FlyUser.java
@@ -1,10 +1,10 @@
 package com.flytecnologia.core.user;
 
 public interface FlyUser {
-    public Long getId();
-    public String getUsername();
-    public String getLogin();
-    public String getPassword();
-    public String getTenant();
-    public String getEmail();
+    Long getId();
+    String getUsername();
+    String getLogin();
+    String getPassword();
+    String getTenant();
+    String getEmail();
 }
