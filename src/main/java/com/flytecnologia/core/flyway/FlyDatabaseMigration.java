90234bda2d353aa78f0453636a6d8e46efafdbe6
Jullierme Barros <julliermegyn@gmail.com>
2018-07-24 19:16:50 -0300

    fix bug when run flyway with multiple schemas

--- src/main/java/com/flytecnologia/core/flyway/FlyDatabaseMigration.java
+++ src/main/java/com/flytecnologia/core/flyway/FlyDatabaseMigration.java
@@ -5,24 +5,18 @@ import org.flywaydb.core.Flyway;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 import java.util.List;
 
 @Service
 public class FlyDatabaseMigration {
     private Flyway flyway;
-    private DataSource dataSource;
-    private EntityManager entityManager;
     private FlyUserService flyUserService;
 
     public FlyDatabaseMigration(Flyway flyway,
                                 DataSource dataSource,
-                                EntityManager entityManager,
                                 FlyUserService flyUserService) {
-        this.entityManager = entityManager;
         this.flyway = flyway;
-        this.dataSource = dataSource;
         this.flyUserService = flyUserService;
         this.flyway.setDataSource(dataSource);
         this.flyway.setLocations("db/migration/common", "db/migration/specific");
@@ -39,14 +33,16 @@ public class FlyDatabaseMigration {
     private void migrateAllSpecificSchemas() {
         List<String> schemas = flyUserService.listAllSchemas();
 
-        if (schemas != null && schemas.size() > 0)
-            migrateSpecificSchema(schemas.toArray(new String[0]));
+        if (schemas != null && schemas.size() > 0) {
+            schemas.forEach(this::migrateSpecificSchema);
+        }
+
     }
 
     /**
      * Used when creating a new schema for a new client
      */
-    public void migrateSpecificSchema(String... schema ) {
+    public void migrateSpecificSchema(String schema) {
         flyway.setSchemas(schema);
         flyway.migrate();
     }
