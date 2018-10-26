1edd76eb430be76cb32ecd3066d1883ff57ef5fd
Jullierme Barros <julliermegyn@gmail.com>
2018-10-25 21:52:24 -0300

    add tenant information interface

--- src/main/java/com/flytecnologia/core/base/plusService/FlyServiceParallelForEach.java
+++ src/main/java/com/flytecnologia/core/base/plusService/FlyServiceParallelForEach.java
@@ -1,28 +1,14 @@
 package com.flytecnologia.core.base.plusService;
 
-import com.flytecnologia.core.base.FlyValidationBase;
-import com.flytecnologia.core.hibernate.multitenancy.FlyMultiTenantConstants;
 import com.flytecnologia.core.hibernate.multitenancy.FlyTenantThreadLocal;
-import com.flytecnologia.core.token.FlyTokenUserDetails;
 
 import java.util.Collection;
 import java.util.function.Consumer;
 
-public interface FlyServiceParallelForEach extends FlyValidationBase {
+public interface FlyServiceParallelForEach extends FlyValidationBase, FlyTenantInformation {
     default <E> void parallelForEach(Collection<E> collection, Consumer<E> consumer) {
-
-        String tenantId = FlyTenantThreadLocal.getTenant();
-        Long userId = FlyTenantThreadLocal.getUserId();
-
-        if (isEmpty(tenantId)) {
-            tenantId = FlyMultiTenantConstants.DEFAULT_TENANT_SUFFIX + FlyTokenUserDetails.getCurrentSchemaName();
-        }
-
-        if (isEmpty(userId))
-            userId = FlyTokenUserDetails.getCurrentUserId();
-
-        String finalTenantId = tenantId;
-        Long finalUserId = userId;
+        String finalTenantId = getTenant();
+        Long finalUserId = getUserId();
 
         collection.parallelStream().forEach(o -> {
 
