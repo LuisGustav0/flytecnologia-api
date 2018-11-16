615f575f20a5c29dace15308c8cb101b25d741df
Jullierme Barros <julliermegyn@gmail.com>
2018-11-16 19:54:26 -0200

    new method: showTheTimeSpent

--- src/main/java/com/flytecnologia/core/base/FlyService.java
+++ src/main/java/com/flytecnologia/core/base/FlyService.java
@@ -27,10 +27,14 @@ import javax.persistence.Basic;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TimeZone;
 
 public abstract class FlyService<T extends FlyEntity, F extends FlyFilter> implements FlyValidationBase,
         FlyTenantInformation {
@@ -456,5 +460,16 @@ public abstract class FlyService<T extends FlyEntity, F extends FlyFilter> imple
         flySwitchTenantService.bindSession();
 
         setTenantInCurrentConnection(tenant);
+    }
+
+    protected void showTheTimeSpent(long start, String message) {
+        long end = System.currentTimeMillis();
+
+        Date date = new Date(end - start);
+        DateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String dateFormatted = formatter.format(date);
+
+        System.out.println(message + dateFormatted);
     }
 }
