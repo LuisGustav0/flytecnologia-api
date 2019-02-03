d724fffbd0f2976c1086fbd099f50803aca1fb1b
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-02-03 13:07:12 -0200

    Add clean array empty values closes #12

--- src/main/java/com/flytecnologia/core/util/FlyArray.java
+++ src/main/java/com/flytecnologia/core/util/FlyArray.java
@@ -1,4 +1,20 @@
 package com.flytecnologia.core.util;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class FlyArray {
+    public static String[] cleanNullValues(String[] value) {
+        if (value == null)
+            return null;
+
+        return Arrays.stream(value)
+                .filter(Objects::nonNull)
+                .filter(FlyArray::isNotEmpty)
+                .toArray(String[]::new);
+    }
+
+    private static boolean isNotEmpty(String string) {
+        return (string != null && !string.isEmpty());
+    }
 }
