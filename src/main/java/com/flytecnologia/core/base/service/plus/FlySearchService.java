87431c88c60463957f632145577daeb281cd2408
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-03-17 15:50:40 -0300

    refectory

--- src/main/java/com/flytecnologia/core/base/service/plus/FlySearchService.java
+++ src/main/java/com/flytecnologia/core/base/service/plus/FlySearchService.java
@@ -5,9 +5,15 @@ import com.flytecnologia.core.search.FlyFilter;
 import com.flytecnologia.core.search.FlyPageableResult;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface FlySearchService<T extends FlyEntity, F extends FlyFilter>
         extends FlyGetRepositoryService<T, F> {
     default FlyPageableResult search(F filter, Pageable pageable) {
         return getRepository().search(filter, pageable);
+    }
+
+    default List<T> search(F filter) {
+        return (List<T>) getRepository().search(filter, null).getResult();
     }
 }
