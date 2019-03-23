7143fb45929ada964e5c0fc98e75ee9c52f92bf9
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-03-23 15:21:25 -0300

    refectory

--- src/main/java/com/flytecnologia/core/base/service/plus/FlyFindService.java
+++ src/main/java/com/flytecnologia/core/base/service/plus/FlyFindService.java
@@ -1,5 +1,6 @@
 package com.flytecnologia.core.base.service.plus;
 
+import com.flytecnologia.core.exception.BE;
 import com.flytecnologia.core.model.FlyEntity;
 import com.flytecnologia.core.search.FlyFilter;
 
@@ -9,6 +10,10 @@ public interface FlyFindService<T extends FlyEntity, F extends FlyFilter> extend
         FlyGetRepositoryService<T, F> {
     default Optional<T> find(Long id) {
         return getRepository().find(id);
+    }
+
+    default T findOrElseThrow(Long id, String msg) {
+        return getRepository().find(id).orElseThrow(() -> new BE(msg));
     }
 
     default Optional<T> find(Long id, String tenant) {
--- src/main/java/com/flytecnologia/core/base/service/plus/FlyTenantService.java
+++ src/main/java/com/flytecnologia/core/base/service/plus/FlyTenantService.java
@@ -9,10 +9,18 @@ import static com.flytecnologia.core.base.service.plus.FlyValidateEmptyService.i
 public interface FlyTenantService<T extends FlyEntity, F extends FlyFilter> extends
         FlyGetRepositoryService<T, F> {
     default void setTenantInCurrentSession(String tenant) {
+        setTenantInCurrentSession(tenant, null);
+    }
+
+    default void setTenantInCurrentSession(String tenant, Long userId) {
         if (isEmpty(tenant))
             return;
 
         FlyTenantThreadLocal.setTenant(tenant);
+
+        if(!isEmpty(userId)) {
+            FlyTenantThreadLocal.setUserId(userId);
+        }
 
         getRepository().setTenantInCurrentConnection(tenant);
     }
