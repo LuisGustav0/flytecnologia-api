c11ec356de3692dba92b7cc2f754f4e158901a3f
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-04-05 22:09:05 -0300

    refectory

--- src/main/java/com/flytecnologia/core/config/FlyMailConfig.java
+++ src/main/java/com/flytecnologia/core/config/FlyMailConfig.java
@@ -1,6 +1,6 @@
 package com.flytecnologia.core.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,12 +10,10 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-
+@AllArgsConstructor
 @Configuration
 @PropertySource("classpath:env/mail.properties")
 public class FlyMailConfig {
-
-    @Autowired
     private Environment env;
 
     @Bean
