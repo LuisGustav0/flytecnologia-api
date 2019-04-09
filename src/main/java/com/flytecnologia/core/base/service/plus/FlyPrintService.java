bc703d6c2630f4ac946150444b6008ddd1762be2
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-04-08 21:08:07 -0300

    refectory

--- src/main/java/com/flytecnologia/core/base/service/plus/FlyPrintService.java
+++ src/main/java/com/flytecnologia/core/base/service/plus/FlyPrintService.java
@@ -1,19 +1,42 @@
 package com.flytecnologia.core.base.service.plus;
 
 import com.flytecnologia.core.exception.BusinessException;
+import com.flytecnologia.core.report.FlyReportService;
 import com.flytecnologia.core.search.FlyFilter;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+import java.util.Map;
+
 public interface FlyPrintService<F extends FlyFilter> {
 
-    default byte[] getReport(F filter) {
+    default void beforePrint(F filter) {
+    }
+
+    Map<String, Object> getParameters(F filter);
+
+    String getReportName();
+
+    default List<?> getProvider(F filter, Map<String, Object> params) {
         return null;
     }
 
+    FlyReportService getReportService();
+
+    default byte[] getReport(F filter) {
+        Map<String, Object> params = getParameters(filter);
+
+        List<?> provider = getProvider(filter, params);
+
+        return getReportService().generatePDF("reports", getReportName(), params, provider);
+    }
+
     default ResponseEntity<ByteArrayResource> print(F filter) {
+        beforePrint(filter);
+
         final byte[] data = getReport(filter);
 
         if (data == null)
