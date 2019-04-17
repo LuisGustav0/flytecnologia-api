bf7ebede73c81dc05b4df5278185a1cf8ce7e227
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-04-16 21:45:22 -0300

    refectory

--- src/main/java/com/flytecnologia/core/base/repository/plus/FlyRecordCountRepository.java
+++ src/main/java/com/flytecnologia/core/base/repository/plus/FlyRecordCountRepository.java
@@ -1,5 +1,6 @@
 package com.flytecnologia.core.base.repository.plus;
 
+import com.flytecnologia.core.base.service.plus.FlyEntityClassService;
 import com.flytecnologia.core.model.FlyEntity;
 import com.flytecnologia.core.search.FlyFilter;
 
@@ -8,14 +9,19 @@ import java.util.Objects;
 import java.util.Optional;
 
 public interface FlyRecordCountRepository<T extends FlyEntity, F extends FlyFilter> extends
-        FlyCreateQueryRepository<T> {
+        FlyEntityManagerRepository,
+        FlyEntityClassService<T> {
 
-    default Optional<Long> getRecordListCount(Long id, String listName) {
+    default Optional<Long> getRecordListCount(Long id, String innerEntityName) {
         final String entityName = getEntityName();
 
         StringBuilder hql = new StringBuilder()
-                .append("select count(entities.id)  \nfrom  ").append(entityName).append(" super  \n")
-                .append("inner join super.").append(listName).append(" as entities \n")
+                .append("select count(entities.id)  \nfrom  ")
+                .append(entityName)
+                .append(" super  \n")
+                .append("inner join super.")
+                .append(innerEntityName)
+                .append(" as entities \n")
                 .append("where super.id = :id\n");
 
         final TypedQuery<Long> query = getEntityManager().createQuery(hql.toString(), Long.class);
