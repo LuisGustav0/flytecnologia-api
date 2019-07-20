cce167540cb59de73b01d26fa941b83e4ecf302c
Ronan Tavares Camargo <ronantav@gmail.com>
2019-07-19 23:21:01 -0300

    Unproxy object after find.

--- src/main/java/com/flytecnologia/core/base/service/plus/FlyFindService.java
+++ src/main/java/com/flytecnologia/core/base/service/plus/FlyFindService.java
@@ -3,13 +3,14 @@ package com.flytecnologia.core.base.service.plus;
 import com.flytecnologia.core.exception.BE;
 import com.flytecnologia.core.model.FlyEntity;
 import com.flytecnologia.core.search.FlyFilter;
+import org.hibernate.Hibernate;
 
 import java.util.Optional;
 
 public interface FlyFindService<T extends FlyEntity, F extends FlyFilter> extends
         FlyGetRepositoryService<T, F> {
     default Optional<T> find(Long id) {
-        return getRepository().find(id);
+        return getRepository().find(id).map(entity -> (T) Hibernate.unproxy(entity));
     }
 
     default T findOrElseThrow(Long id, String msg) {
