cdad66ed87c4d2e0d3a8652524f93019e6001544
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-09-24 15:18:20 -0300

    correct create body/response

--- src/main/java/com/flytecnologia/core/base/controller/plus/FlyCreateController.java
+++ src/main/java/com/flytecnologia/core/base/controller/plus/FlyCreateController.java
@@ -2,32 +2,27 @@ package com.flytecnologia.core.base.controller.plus;
 
 import com.flytecnologia.core.model.FlyEntity;
 import com.flytecnologia.core.search.FlyFilter;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 
 public interface FlyCreateController<T extends FlyEntity, F extends FlyFilter> extends
         FlyGetServiceController<T, F> {
-    default void addHeaderLocationForCreation(HttpServletResponse response, Long id) {
-        final URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
-                .buildAndExpand(id).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-    }
 
     @PostMapping
     @PreAuthorize("hasAuthority(getAuthorityCreate()) and #oauth2.hasScope('write')")
-    default ResponseEntity<T> create(@RequestBody T entity,
-                                     HttpServletResponse response) {
+    default ResponseEntity<Void> create(@RequestBody T entity) {
         entity = getService().create(entity);
 
-        addHeaderLocationForCreation(response, entity.getId());
+        return ResponseEntity.created(getLocation(entity.getId())).build();
+    }
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(entity);
+    default URI getLocation(Long id) {
+        return ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(id).toUri();
     }
 }
