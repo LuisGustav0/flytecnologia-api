39cceeb73bf075f875f5da64f082b410c0510a11
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-09-25 09:35:30 -0300

    return the id to navigate to another page

--- src/main/java/com/flytecnologia/core/base/controller/plus/FlyCreateController.java
+++ src/main/java/com/flytecnologia/core/base/controller/plus/FlyCreateController.java
@@ -2,6 +2,7 @@ package com.flytecnologia.core.base.controller.plus;
 
 import com.flytecnologia.core.model.FlyEntity;
 import com.flytecnologia.core.search.FlyFilter;
+import lombok.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,20 +10,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public interface FlyCreateController<T extends FlyEntity, F extends FlyFilter> extends
         FlyGetServiceController<T, F> {
 
     @PostMapping
     @PreAuthorize("hasAuthority(getAuthorityCreate()) and #oauth2.hasScope('write')")
-    default ResponseEntity<Void> create(@RequestBody T entity) {
+    default ResponseEntity<Object> create(@RequestBody T entity) {
         entity = getService().create(entity);
 
-        return ResponseEntity.created(getLocation(entity.getId())).build();
+        return ResponseEntity
+                .created(getLocation(entity.getId()))
+                .body(getResponseBodyWhenCreate(entity));
     }
 
-    default URI getLocation(Long id) {
+    default URI getLocation(@NonNull Long id) {
         return ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
                 .buildAndExpand(id).toUri();
+    }
+
+    default Object getResponseBodyWhenCreate(@NonNull T entity) {
+        Map<String, Long> response = new HashMap<>();
+        response.put("id", entity.getId());
+
+        return response;
     }
 }
